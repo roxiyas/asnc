7a7d34d5023876831114677cd6f79610431d15fd
siledth <siledth@gmail.com>
2020-12-07 23:34:03 -0430

    insert de usuario y validacion

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,7 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
-
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model("User_model");
+	}
 	public function index(){
 		$this->load->view('templates/header.php');
         $this->load->view('templates/navigator.php');
@@ -16,7 +20,7 @@ class User extends CI_Controller {
 		$password = $this->input->post("password");
 		$repeatPassord = $this->input->post("repeatPassord");
 
-		$this->form_validation->set_rules('nombre', 'Nombre completo', 'required|min_length[3]');
+		$this->form_validation->set_rules('nombre', 'Nombre completo', 'required|min_length[6]');
 		$this->form_validation->set_rules('email', 'Correo eléctronico', 'required|valid_email|is_unique[usuarios.email]');
 		$this->form_validation->set_rules('password', 'Password', 'required|max_length[10]|min_length[8]|alpha_numeric');
 		$this->form_validation->set_rules('repeatPassord', 'Confirma contraseña', 'required|matches[password]');
@@ -29,20 +33,21 @@ class User extends CI_Controller {
 		}else{
 			$data = array(
 				"nombre"=>$nombre,
-				"usuario"=>$nombre,
 				"password"=>md5($password),
+				"email" => $email,
+				"foto"=>2,
 				"perfil"=>1,
 				"estado"=>1,
 				"ultimo_login"=>date("Y-m-d h:m:s"),
 				"fecha"=>date("Y-m-d"),
 				"intentos"=>1,
-				"unidad"=>1,
-				"email" => $email
+				"unidad"=>1
+				
 			);
-
 			$this->User_model->save($data);
-			$this->session->set_flashdata('sa-success2', 'Se guardo los datos correctamente');
+			$this->session->set_flashdata('success', 'Se guardo los datos correctamente');
 			redirect('user/index');
+			
 		}
 	}
 }
--- application/views/user/add.php
+++ application/views/user/add.php
@@ -9,7 +9,7 @@
 					<h4 class="panel-title">Nuevo Usuario</h4>
 				</div>
 				<div class="panel-body">
-					<form action="<?=base_url()?>index.php/user/save" class="form-horizontal" data-parsley-validate="true" name="demo-form">
+					<form action="<?=base_url()?>index.php/User/save" class="form-horizontal" data-parsley-validate="true" name="demo-form" method="POST">
                         <div class="row">
                             <div class="form-group col-3">
                                 <label>Perfil</label>
@@ -38,30 +38,30 @@
                             <div class="form-group col-6">
                                 <label>Nombre completo</label>
                                 <input type="text" name="nombre" class="form-control <?php echo form_error('nombre') ? 'is-invalid':'' ; ?>" placeholder="Nombre completo" value="<?php echo set_value('nombre'); ?>">
-                                <div class="invalid-feedback">
-                                    <?php echo form_error('nombre'); ?>
-                                </div>
+                                    <div class="invalid-feedback">
+                                        <?php echo form_error('nombre'); ?>
+                                    </div>
                             </div>
                             <div class="form-group col-6">
-                                <label>Email</label>
+                                <label>Correo Institucional</label>
                                 <input type="text" name="email" class="form-control <?php echo form_error('email') ? 'is-invalid':'' ; ?>" aria-describedby="emailHelp" placeholder="Correo eléctronico" value="<?php echo set_value('email'); ?>">
-                                <div class="invalid-feedback">
-                                    <?php echo form_error('email'); ?>
-                                </div>
+                                    <div class="invalid-feedback">
+                                        <?php echo form_error('email'); ?>
+                                    </div>
                             </div>
                             <div class="form-group col-6">
                                     <label for="exampleInputPassword1">Contraseña</label>
                                     <input type="password" name="password" class="form-control <?php echo form_error('password') ? 'is-invalid':'' ; ?>"placeholder="Contraseña" value="<?php echo set_value('password'); ?>">
-                                    <div class="invalid-feedback">
-                                        <?php echo form_error('password'); ?>
-                                    </div>
+                                        <div class="invalid-feedback">
+                                            <?php echo form_error('password'); ?>
+                                        </div>
                             </div>
                             <div class="form-group col-6">
                                 <label for="exampleInputPassword1">Repite la contraseña</label>
                                 <input type="password" name="repeatPassord" class="form-control <?php echo form_error('repeatPassord') ? 'is-invalid':'' ; ?>" placeholder="Contraseña" value="">
-                                <div class="invalid-feedback">
-                                    <?php echo form_error('repeatPassord'); ?>
-                                </div>
+                                    <div class="invalid-feedback">
+                                        <?php echo form_error('repeatPassord'); ?>
+                                    </div>
                             </div>
                         </div>
 						<div class="form-group col 12 text-center">
@@ -73,6 +73,18 @@
         </div>
     </div>
 </div>
-<?php if ($this->session->flashdata('sa-success2')) { ?>
-    <div hidden id="sa-success2"> <?= $this->session->flashdata('sa-success2') ?> </div>
-<?php } ?>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+
+    <script>
+        
+        <?php if($this->session->flashdata("success")): ?>
+
+            Swal.fire({
+                icon: 'success',
+                title: 'Bien......',
+                text: '<?php echo $this->session->flashdata("success"); ?>',
+            });
+        <?php endif; ?>
+
+    </script>
