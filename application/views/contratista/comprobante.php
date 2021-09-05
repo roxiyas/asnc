f4187f191baddb2a68751eaa42ddb67a8886f6de
roxi <roxiyas.30@mail.com>
2021-09-05 19:12:34 -0400

    Arreglo

--- application/views/contratista/comprobante.php
+++ application/views/contratista/comprobante.php
@@ -4,118 +4,121 @@
         <div class="col-6 mb-3">
             <a class="btn btn-circle waves-effect btn-lg waves-circle waves-float btn-primary" href="javascript:history.back()"> Volver</a>
         </div>
-        <div class="col-lg-12" id="imp1" >
-         <div class="col-10 pull-left pull-left">
+        <div class="col-lg-12 panel mt-3" id="imp1" >
+            <div class="row">
+                <div class="col-10 pull-left">
+        	           <img src="<?php echo base_url('Plantilla/img/membretesnc.png'); ?>" height="50"  />
+                </div>
+                <div class="pull-right">
+                    <img src="<?php echo base_url('Plantilla/img/logosnc.png'); ?>" height="80"  />
+                </div>
+            </div>
+                <div class="row mt-3">
+                    <div class="col-12 text-right">
+                        <h4><b>Nro. Correlativo: </b><?= $consulta['nro_correlativo'] ?></h4>
+                    </div>
+                    <div class="col-12 text-center mt-3">
+                        <h3><b>COMPROBANTE DE INSCRIPCIÓN</b></h3>
+                        <h3><b>EN EL REGISTRO NACIONAL DE CONTRATISTAS</b></h3>
+                    </div>
+                    <div class="col-12 text-center mt-3">
+                        <h4>Este Registro Nacional de Contratistas, hace constar que:</h4>
+                    </div>
+                    <div class="col-12 text-center mt-3 mb-3">
+                        <h3><b>INFORMACIÓN DEL CONTRATISTA</b></h3>
+                    </div>
+                    <div class="col-2"></div>
+                    <div class="col-2 text-right">
+                        <h4><b>Razón Social:</b></h4>
+                    </div>
+                    <div class="col-8">
+                        <h4><?=$consulta['nombre']?></h4>
+                    </div>
 
-	          <img src="<?php echo base_url('Plantilla/img/membretesnc.png'); ?>" height="50"  />
+                    <div class="col-2"></div>
+                    <div class="col-2 text-right">
+                        <h4><b>RIF:</b></h4>
+                    </div>
+                    <div class="col-6">
+                        <h4><?=$consulta['rifced']?></h4>
+                    </div>
+                    <div class="col-12 text-justify mb-3 mt-3">
+                    <h4>Se encuentra HABILITADO para contratar con la Administración Pública Nacional, Estadal y Municipal en adquisición de bienes, servicios y ejecución de obras,
+                        de conformidad con lo previsto en el artículo 8 de la Ley Constitucional Contra la Guerra Económica para la Racionalidad y Uniformidad en la Adquisición de Bienes,
+                        Servicios y Obras Públicas, publicada en Gaceta Oficial de la República Bolivariana de Venezuela N° 40.398, de fecha 11 de enero de 2018 en concordancia con el artículo
+                        47 del Decreto con Rango, Valor y Fuerza de Ley de Contrataciones Públicas, publicado en Gaceta Oficial de la República Bolivariana de Venezuela Extraordinario N° 6.154,
+                        de fecha 19 de noviembre de 2014, para el período:</h4>
+                    </div>
+                    <div class="col-2"></div>
+                    <div class="col-2 text-right">
+                        <h4><b>Desde:</b></h4>
+                    </div>
+                    <div class="col-8">
+                        <h4><?=$consulta['fecinscrnc_at2']?></h4>
+                    </div>
+                    <div class="col-2"></div>
+                    <div class="col-2 text-right">
+                        <h4><b>Hasta:</b></h4>
+                    </div>
+                    <div class="col-8">
+                        <h4><?=$consulta['fecvencrnc_at2']?></h4>
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-3 text-right">
+                        <h4><b>Nº de Comprobante:</b></h4>
+                    </div>
+                    <div class="col-8">
+                        <h4><?=$consulta['numcertrnc2']?></h4>
+                    </div>
+                    <div class="col-12 mt-3 mb-5 text-justify">
+                        <h4>El contratista acepta someterse a lo establecido en el artículo 10 de la Ley Constitucional Contra la Guerra Económica para la Racionalidad y
+                            Uniformidad en la Adquisición de Bienes, Servicios y Obras Públicas, el cual establece lo siguiente. <b>Artículo 10. Las personas naturales o jurídicas
+                            que presten declaraciones falsas para obtener el comprobante de inscripción a que se refiere el artículo precedente serán inhabilitados para contratar
+                            con el estado, por un plazo de diez (10) años.</b> Si la inhabilitación recae sobre sujetos que poseen contrataciones en ejecución con el Estado, estas serán
+                            susceptibles de resolución unilateral por causas imputables al contratista o nulidad de esos contratos por parte del contratante, con fundamento a lo establecido
+                            en el Decreto con Rango, Valor y Fuerza de Ley de Contrataciones Públicas.</td>
+                        </h4>
+                    </div>
+                    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+
+                    <div class="col-12 text-center mt-5">
+                        <h4>Anthoni Camilo Torres</h4>
+                        <h4>Director General</h4>
+                        <h6>"Resolución CCP/DGCJ N° 001/2014 de fecha 07 de enero de 2014, Publicada <br>
+                            en la Gaceta Oficial de la República Bolivariana de Venezuela <br>
+                            Nº 40.334 de fecha 15 de enero de 2014."</h6>
+                    </div>
+                    <div class="col-12 text-right mt-4">
+                        <h4> <b>Fecha de Consulta: </b><?php echo date("d-m-Y"); ?></h4>
+                    </div>
+                    <br><br><br><br><br><br><br><br>
+                    <div class="col-12 mt-4">
+                        <h6>Firma electrónica de datos consultados: <br>
+                        XaN-cixoSyPa5Kyop8k-Ac7TzWROZ4iUzQmlhcayO9eGIi-9964 <br>
+                        a132BUqtwdDIIDdT8BxYlAjIsd61Wnsqobpb01742NPUUjM2J21BhdUOcoRd3sZELb8yx5fAw+k5ch8- <br>
+                        Firmado electrónico por Anthoni Camilo Torres, avalado por la autoridad certificadora Fundación Instituto <br>
+                        de Ingeniería, adscrito a SUSCERTE <br>
+                        La validez del presente certificado debe ser consultado en la dirección electrónica www.snc.gob.ve y se <br>
+                        exhorta a todos los Órganos y Entes del Estado responsables de las contrataciones públicas a imprimir <br>
+                        un ejemplar a objeto de ser incorporado al expediente de la contratación o concurso a ejecutar.</h6>
+                    </div>
+                </div>
+            </div>
         </div>
-
-            <div class="panel panel-inverse">
-
-                <br>
-                <table class="table  style="border: hidden"">
-                    <tbody>
-                      <tr>
-                      <td align="right" colspan="2" style="border: hidden">Nro Correlativo
-                      </h5></td>
-                        <td align="right"  style="border: hidden">
-                          <h5><?= $consulta['nro_correlativo'] ?></h5>
-                    </tr>
-                    <tr>
-                    <td align="center" colspan="3"  style="border: hidden"> <strong>COMPROBANTE DE INSCRIPCIÓN EN EL REGISTRO NACIONAL DE CONTRATISTAS </strong></td>
-                  </tr>
-                  <tr>
-                  <td align="center" colspan="2"  style="border: hidden">Este Registro Nacional de Contratistas, hace constar que: </td>
-                  <td align="rigth"   style="border: hidden"> <img src="<?php echo base_url('Plantilla/img/logosnc.png'); ?>" height="80"  /> </td>
-                </tr>
-                <tr>
-                <td align="center" colspan="3"  style="border: hidden"><strong>INFORMACIÓN DEL CONTRATISTA </strong></td>
-              </tr>
-  <tr class="odd gradeX" style="text-align:center" >
-                            <th style="border: hidden">Razón Social:</th>
-                            <td style="border: hidden"><?=$consulta['nombre']?> </td>
-                        </tr>
-                        <tr class="odd gradeX" style="text-align:center">
-                            <th style="border: hidden">RIF:</th>
-                            <td style="border: hidden"><?=$consulta['rifced']?> </td>
-                        </tr>
-                        <tr>
-
-
-                				<td align="justify" colspan="3">Se encuentra HABILITADO para contratar con la Administración Pública Nacional, Estadal y Municipal en adquisición de bienes, servicios y ejecución de obras, de conformidad con lo previsto en el artículo 8 de la Ley Constitucional Contra la Guerra Económica para la Racionalidad y Uniformidad en la Adquisición de Bienes, Servicios y Obras Públicas, publicada en Gaceta Oficial de la República Bolivariana de Venezuela N° 40.398, de fecha 11 de enero de 2018 en concordancia con el artículo 47 del Decreto con Rango, Valor y Fuerza de Ley de Contrataciones Públicas, publicado en Gaceta Oficial de la República Bolivariana de Venezuela Extraordinario N° 6.154, de fecha 19 de noviembre de 2014, para el período:</td>
-                			</tr>
-                      <tr class="odd gradeX" style="text-align:center" style="border: hidden">
-                          <th style="border: hidden">Desde:</th>
-                          <td style="border: hidden"><?=$consulta['fecinscrnc_at2']?> </td>
-                      </tr>
-                      <tr class="odd gradeX" style="text-align:center" style="border: hidden">
-                          <th>Hasta:</th>
-                          <td><?=$consulta['fecvencrnc_at2']?> </td>
-                      </tr>
-                      <tr class="odd gradeX" style="text-align:center" style="border: hidden">
-                          <th style="border: hidden" >N° de Comprobante:</th>
-                          <td style="border: hidden"><?=$consulta['numcertrnc2']?> </td>
-                      </tr>
-                      <tr>
-                      <td align="justify" colspan="3">El contratista acepta someterse a lo establecido en el artículo 10 de la Ley Constitucional Contra la Guerra Económica para la Racionalidad y Uniformidad en la Adquisición de Bienes, Servicios y Obras Públicas, el cual establece lo siguiente. Artículo 10. Las personas naturales o jurídicas que presten declaraciones falsas para obtener el comprobante de inscripción a que se refiere el artículo precedente serán inhabilitados para contratar con el estado, por un plazo de diez (10) años. Si la inhabilitación recae sobre sujetos que poseen contrataciones en ejecución con el Estado, estas serán susceptibles de resolución unilateral por causas imputables al contratista o nulidad de esos contratos por parte del contratante, con fundamento a lo establecido en el Decreto con Rango, Valor y Fuerza de Ley de Contrataciones Públicas.</td>
-                    </tr>
-                    <tr>
-                    <td align="center" colspan="3" style="border: hidden">Anthoni Camilo Torres.</td>
-                  </tr>
-                  <tr>
-                  <td align="center" colspan="3" style="border: hidden">Director General.</td>
-                </tr>
-                <tr>
-                <td align="center" colspan="3" style="border: hidden">"Resolución CCP/DGCJ N° 001/2014 de fecha 07 de enero de 2014, Publicada
-                </td>
-              </tr>
-              <tr>
-                <td align="center" colspan="3" style="border: hidden"> en la Gaceta Oficial de la República Bolivariana de Venezuela
-                </td>
-              </tr>
-              <tr>
-                <td align="center" colspan="3" style="border: hidden"> N° 40.334 de fecha 15 de enero de 2014"
-                </td>
-              </tr>
-                                </tbody>
-                </table>
-                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-                <div class="form-group col-3 pull-right pull-right">
-                    <label>Fecha de Consulta:</label>
-                    <h5><?php echo date("d-m-Y"); ?></h5>
-                </div>
-<br><br><br>
-
-                <div class="form-group col-3 ">
-                    <label>Firma electrónica de datos consultados:
-XaN-cixoSyPa5Kyop8k-Ac7TzWROZ4iUzQmlhcayO9eGIi-9964
-a132BUqtwdDIIDdT8BxYlAjIsd61Wnsqobpb01742NPUUjM2J21BhdUOcoRd3sZELb8yx5fAw+k5ch8-
-Firmado electrónico por Anthoni Camilo Torres, avalado por la autoridad certificadora Fundación Instituto
-de Ingeniería, adscrito a SUSCERTE
-La validez del presente certificado debe ser consultado en la dirección electrónica www.snc.gob.ve y se
-exhorta a todos los Órganos y Entes del Estado responsables de las contrataciones públicas a imprimir
-un ejemplar a objeto de ser incorporado al expediente de la contratación o concurso a ejecutar.
-</label>
-
-                </div>
-
-
-      </div>
-      </div>
     </div>
     <div class="form-group col 12 text-center">
         <button class="btn btn-default mt-1 mb-1" type="button" id="print" onclick="printContent('imp1');" >Imprimir </button>
-
-
     </div>
-  </div>
-            <script src="<?= base_url() ?>/js/contratista/contratista.js"></script>
-            <script>
-          function printContent(imp1){
-          var restorepage = $('body').html();
-          var printcontent = $('#' + imp1).clone();
-          $('body').empty().html(printcontent);
-          window.print();
-          $('body').html(restorepage);
-          }
-          </script>
+</div>
+<script src="<?= base_url() ?>/js/contratista/contratista.js"></script>
+<script>
+    function printContent(imp1){
+        var restorepage = $('body').html();
+        var printcontent = $('#' + imp1).clone();
+            $('body').empty().html(printcontent);
+            window.print();
+                $('body').html(restorepage);
+    }
+</script>
--- application/views/contratista/infor_contratista.php
+++ application/views/contratista/infor_contratista.php
@@ -20,19 +20,15 @@
             </div>
         </div>
         <div class="col-lg-12" style="display: none" id="items">
-
             <div class="panel panel-inverse">
                 <div class="panel-heading">
                     <h4 class="panel-title text-center"><b>Información del Contratista </b></h4>
                 </div>
                 <div class="panel-body" id="no_existe">
-
                     <div class="row">
                         <div class="col-md-12 mt-2 mb-2">
                             <h4 class="mt-2"><label>Por Favor revise el Rif ingresado! y vuelva a intentar.</label></h4>
-
                         </div>
-
                     </div>
                 </div>
                 <div class="panel-body" id="existe">
@@ -42,8 +38,6 @@
                                 <input class="form-control" type="hidden" name="idedocontratistas" id="idedocontratistas">
                                 <textarea class="form-control" name="descedocont" id="descedocont" rows="4" readonly></textarea>
                             </div>
-
-
                             <div class="form-group col-12 text-center">
                                 <h4 class="panel-title"> <b> Información de la Empresa </b> </h4>
                             </div>
@@ -72,8 +66,7 @@
                             <br>
                             <div class="form-group col-3" >
                                 <label>Dirección Fiscal</label>
-<textarea class="form-control" name="dirfiscal" id="dirfiscal" rows="3" readonly></textarea>
-
+                                    <textarea class="form-control" name="dirfiscal" id="dirfiscal" rows="3" readonly></textarea>
                             </div>
                             <div class="form-group col-3">
                                 <label>Teléfonos</label>
@@ -113,11 +106,9 @@
                         </div>
                         <div class="form-group col 12 text-center">
                             <button class="btn btn-default mt-1 mb-1"  type="submit" class="send"> Ver Planilla Resumen</button>
-                            <input class="btn btn-default" type="submit" value="Ver Comprobante RNC" formaction="<?= base_url() ?>index.php/Contratista/ver_comprobante"/>
-
-            							</div>
+                            <input class="btn btn-default" type="submit" value="Ver Comprobante RNC" formaction="<?= base_url() ?>index.php/Contratista/ver_comprobante"/></div>
                         </div>
-                      
+
                     </form>
                 </div>
             </div>
@@ -128,5 +119,9 @@
     </div>
 </div>
 </div>
-
-<script src="<?= base_url() ?>/js/contratista/contratista.js"></script>
+<script type="text/javascript">
+function mayusculas(e) {
+    e.value = e.value.toUpperCase();
+}
+</script>
+<script src="<?=base_url()?>/js/contratista/contratista.js"></script>
